6cc4aea4b45a7f00bd255d29f4eeaad26c7e953c
alain <cuban.quant@gmail.com>
2022-09-07 21:13:43 -0400

    assingment 2 more than simple skeleton

--- assignment2.py
+++ assignment2.py
@@ -1,5 +1,6 @@
 import datetime
-import requests
+import argparse
+import urllib.request
 
 
 def downloadData(url):
@@ -10,9 +11,12 @@ def downloadData(url):
     :return:
     """
     # read the URL
-    response = requests.get(url)
+    # pip install requests
+    with urllib.request.urlopen(url) as response:
+        response = response.read().decode('utf-8')
+
     # return the data
-    return response.text
+    return response
 
 
 def processData(data):
@@ -25,30 +29,62 @@ def processData(data):
     result = {}
     # processing here ...
     lines = data.split("\n")
+    header = True
     for line in lines:
         # process each line
-        elements = line.split(",")
-        # id =
-        # name =
-        # date_str =
+        # Skip the header
+        if header:
+            header = False
+            continue
 
-        # result[id] = (name, birthday)
+        # Skip blank lines
+        if len(line) == 0:
+            continue
+
+        elements = line.split(",")
+        id = int(elements[0])
+        name = elements[1]
+        date_str = elements[2]
+
+        try:
+            birthday = datetime.datetime.strptime(date_str, "%d/%m/%Y")
+            result[id] = (name, birthday)
+        except ValueError as e:
+            # add this error line to the logger....
+            print(f"Error parsing {line}")
+
     return result
 
 
 def displayPerson(id, personData):
-    # search for id in persondData and do something about it
-    pass
+    # search for id in persondData and print it nicely
+    print(personData[id])
 
 
-if __name__ == "__main__":
-    url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
+def main(url):
+    """
+    Main function
+
+    :param url:
+    :return:
+    """
     # Do something
     data = downloadData(url)
     personData = processData(data)
-    # ask use for ID
+
+    # ask user for ID
     id = 33
     displayPerson(id, personData)
 
 
+if __name__ == "__main__":
+    # https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv
+    # Create the parser
+    my_parser = argparse.ArgumentParser(description='Assignment2 Parser')
+    # Add the arguments
+    my_parser.add_argument('--url', type=str, required=True, help='The URL we want to download')
+    # Execute the parse_args() method
+    args = my_parser.parse_args()
 
+    url = args.url
+    main(url)
